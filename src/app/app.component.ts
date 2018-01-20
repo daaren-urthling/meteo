1af398723083120831a84651d5f9b38f1864f0ab
daaren-urthling <daaren.urthling@hotmail.com>
2018-01-20 17:24:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const API_URL = 'http://api.openweathermap.org/data/2.5';
+const API_KEY = '&appid=de80ca74281ccc48b732698bfed205d1';
+
+import { Result } from './Result';
+import { City } from './city';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Meteo';
+  result: Result = new Result();
+  city : string;
+  error: boolean = false;
+  favorites: City[] = [
+    new City("Genova", 3176219),
+    new City("Savona", 6542009),
+    new City("La Spezia",6540156),
+    new City("Imperia", 3175532)
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+  OnGetForecast(query: string) {
+    this.error = false;
+    this.http.get(API_URL + '/weather?' + query + '&units=metric&lang=it' + API_KEY).subscribe((data:any) => {
+      // Read the result field from the JSON response.
+      this.result.id = data.id;
+      this.result.city = data.name;
+      this.result.temperature = data.main.temp;
+      this.result.tempMin = data.main.temp_min;
+      this.result.tempMax = data.main.temp_max;
+      this.result.condition = data.weather[0].description;
+      this.result.icon = data.weather[0].icon;
+    },
+    (error:any) => {
+      this.error = true;
+      this.result = new Result();
+    });    
+  }
+
+  OnGetForecastByName() {
+    this.OnGetForecast('q=' + this.city);
+  }
+
+  OnGetForecastByID(cityId: number) {
+    this.OnGetForecast('id=' + cityId);
+  }
+
+  OnAddFavorites() {
+    this.favorites.push(new City(this.result.city, this.result.id));
+  }
+
+  OnRemoveFavorite(i: number) {
+    this.favorites.splice(i,1);
+  }
 }
